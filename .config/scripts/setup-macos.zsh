101b647caf46c2ab536371f9c8e3f6cfe2dcf887
Alex Shan <shantong.sir@gmail.com>
2024-12-09 22:57:02 +0800

    feat: update setup-macos.zsh

--- .config/scripts/setup-macos.zsh
+++ .config/scripts/setup-macos.zsh
@@ -4,9 +4,6 @@ set -e
 
 DOTFILES_REPO_URL="https://github.com/AlexShan2008/dotfiles.git"
 DOTFILES_BARE_DIR="$HOME/.dotfiles"
-FONT_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/InconsolataLGC.zip"
-FONT_TEMP_DIR="/tmp/InconsolataLGC"
-FONT_TARGET_DIR="$HOME/Library/Fonts"
 
 log() {
   local level="$1"
@@ -72,42 +69,46 @@ restore_dotfiles() {
 }
 
 setup_ohmyzsh() {
-  log "INFO" "Installing Oh My Zsh..."
-  
+  log "INFO" "Starting Oh My Zsh setup..."
+
   if [[ -d "$HOME/.oh-my-zsh" ]]; then
-    log "INFO" "Oh My Zsh is already installed, skipping..."
+    log "INFO" "Oh My Zsh is already installed, skipping installation."
   else
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    log "INFO" "Oh My Zsh is not installed, proceeding with installation..."
+    if ! sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended; then
+      log "ERROR" "Failed to install Oh My Zsh. Exiting setup."
+      return 1
+    fi
+    log "INFO" "Oh My Zsh installed successfully."
   fi
 
-  log "INFO" "Installing Zsh plugins..."
-  local plugins=("zsh-autosuggestions" "zsh-completions" "fast-syntax-highlighting")
-  local plugin_url_base="https://github.com/zsh-users"
-  local plugin_dir="$HOME/.zsh/plugins"
+  local ZSH_PLUGINS_PREFIX="$HOME/.zsh/plugins"
+  local plugins=(
+    "zsh-users/zsh-autosuggestions"
+    "zsh-users/zsh-completions"
+    "zdharma-continuum/fast-syntax-highlighting"
+  )
 
-  mkdir -p "$plugin_dir"
-  for plugin in "${plugins[@]}"; do
-    if [[ ! -d "$plugin_dir/$plugin" ]]; then
-      git clone "${plugin_url_base}/${plugin}.git" "$plugin_dir/$plugin"
+  log "INFO" "Preparing to install Zsh plugins..."
+  mkdir -p "$ZSH_PLUGINS_PREFIX"
+
+  for plugin_repo in "${plugins[@]}"; do
+    local plugin_name=$(basename "$plugin_repo")
+    local plugin_path="$ZSH_PLUGINS_PREFIX/$plugin_name"
+
+    if [[ -d "$plugin_path" ]]; then
+      log "INFO" "Plugin $plugin_name is already installed, skipping."
+    else
+      log "INFO" "Cloning plugin $plugin_name from https://github.com/$plugin_repo..."
+      if ! git clone "https://github.com/$plugin_repo.git" "$plugin_path"; then
+        log "ERROR" "Failed to clone plugin $plugin_name. Skipping this plugin."
+        continue
+      fi
+      log "INFO" "Plugin $plugin_name installed successfully."
     fi
   done
-}
 
-install_fonts() {
-  log "INFO" "Installing Inconsolata LGC Nerd Font..."
-
-  if ls "${FONT_TARGET_DIR}"/*InconsolataLGCNerdFontMono* 1> /dev/null 2>&1; then
-    log "INFO" "Fonts already installed, skipping..."
-    return
-  fi
-
-  mkdir -p "${FONT_TEMP_DIR}"
-  curl -L "$FONT_DOWNLOAD_URL" -o "/tmp/InconsolataLGC.zip"
-  unzip "/tmp/InconsolataLGC.zip" -d "$FONT_TEMP_DIR"
-  cp "${FONT_TEMP_DIR}"/*InconsolataLGCNerdFontMono*.ttf "$FONT_TARGET_DIR"
-  rm -rf "/tmp/InconsolataLGC.zip" "$FONT_TEMP_DIR"
-
-  log "INFO" "Fonts installed successfully."
+  log "INFO" "Oh My Zsh setup complete!"
 }
 
 install_homebrew() {
@@ -123,15 +124,186 @@ install_homebrew() {
   eval "$(/opt/homebrew/bin/brew shellenv)"
 }
 
+install_homebrew_packages() {
+  log "INFO" "Installing Homebrew packages..."
+
+  local brewfile="$HOME/Brewfile"
+
+  if [[ ! -f "$brewfile" ]]; then
+    log "INFO" "No Brewfile found at $brewfile"
+    log "INFO" "Skipping package installation"
+    return 0
+  fi
+
+  local current_locale=$(defaults read NSGlobalDomain AppleLocale 2>/dev/null || echo en_CN)
+
+  local brew_bundle_status
+
+  # Temporarily set locale to en_US for mas-cli compatibility
+  # Reference: https://github.com/mas-cli/mas/blob/main/Sources/mas/Controllers/ITunesSearchAppStoreSearcher.swift#L18-L22
+  defaults write NSGlobalDomain AppleLocale -string en_US
+
+  # Run brew bundle
+  if brew bundle --file="$brewfile"; then
+    log "INFO" "Brewfile successfully installed"
+    brew_bundle_status=0
+  else
+    log "ERROR" "Brewfile installation failed"
+    log "ERROR" "You may want to run 'brew bundle' manually later"
+    brew_bundle_status=1
+  fi
+
+  # Restore original locale setting
+  defaults write NSGlobalDomain AppleLocale -string "$current_locale"
+
+  return $brew_bundle_status
+}
+
+install_nodejs() {
+  log "INFO" "==========================================================="
+  log "INFO" "               Setting up Node.js Environment              "
+  log "INFO" "-----------------------------------------------------------"
+
+  if command -v proto > /dev/null; then
+    echo "proto is already installed, skipping..."
+  else
+    echo "Installing proto..."
+    if ! curl -fsSL https://moonrepo.dev/install/proto.sh | bash -s -- --no-profile --yes; then
+      echo "Error: Failed to install proto. Exiting..." >&2
+      return 1
+    fi
+  fi
+
+  log "INFO" "Installing Node.js LTS and pnpm..."
+
+  if ! proto install node; then
+    log "ERROR" "Failed to install Node.js. Exiting..." >&2
+    return 1
+  fi
+
+  log "INFO" "Node.js Version: $(proto run node -- --version)"
+  log "INFO" "* Installing pnpm..."
+
+  if ! proto install pnpm; then
+    log "ERROR" "Failed to install pnpm. Exiting..." >&2
+    return 1
+  fi
+  log "INFO" "pnpm installed successfully!"
+  log "INFO" "pnpm Version: $(proto run pnpm -- --version)"
+
+  log "INFO" "Configuring npm global packages..."
+  
+  export NPM_CONFIG_PREFIX="$HOME/.npm-global"
+  
+  mkdir -p "$NPM_CONFIG_PREFIX"
+
+  local npm_global_pkgs=(
+    @upimg/cli
+    0x
+    npm-why
+  )
+
+  for pkg in "${npm_global_pkgs[@]}"; do
+    log "INFO" "Installing: $pkg"
+    if ! proto run npm -- install -g "$pkg"; then
+      log "WARNING" "Failed to install $pkg" >&2
+    fi
+  done
+
+  log "INFO" "Node.js Environment Setup Complete!"
+}
+
+install_rust() {
+  log "INFO" "==========================================================="
+  log "INFO" "                      Install Rust                         "
+  log "INFO" "-----------------------------------------------------------"
+
+  if command -v rustc > /dev/null; then
+    echo "Rust is already installed, skipping..."
+  else
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  fi
+}
+
+reload_zshrc() {
+  log "INFO" "==========================================================="
+  log "INFO" "                   Reload Alex's Environment zshrc                  "
+  log "INFO" "-----------------------------------------------------------"
+
+  if [[ ! -f "$HOME/.zshrc" ]]; then
+    log "ERROR" "No .zshrc file found. Exiting."
+    return 1
+  fi
+
+  log "WARNING" "Removing .zcompdump files in $HOME..."
+  rm -f "$HOME/.zcompdump"*
+
+  log "INFO" "Sourcing .zshrc..."
+  autoload -Uz compinit && compinit -i
+
+  log "INFO" "Reloading zsh configuration..."
+  exec zsh
+}
+
+setup_iterm2() {
+  log "INFO" "==========================================================="
+  log "INFO" "                   Setting up iTerm2...                    "
+  log "INFO" "-----------------------------------------------------------"
+
+  if [ ! -d "/Applications/iTerm.app" ]; then
+    log "ERROR" "iTerm2 is not installed, skipping configuration..."
+    return 0
+  fi
+
+  local iterm2_default_config="$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+
+  if [ ! -f "$iterm2_default_config" ]; then
+    log "ERROR" "Default iTerm2 config file not found: $iterm2_default_config"
+    return 0
+  fi
+
+  log "INFO" "Setting iTerm2 to use its default config location..."
+
+  defaults write -app iTerm PrefsCustomFolder ""
+  defaults write -app iTerm LoadPrefsFromCustomFolder -bool false
+
+  log "INFO" "iTerm2 configuration completed using default settings."
+}
+
+
+display_todo_list() {
+  log "INFO" "==========================================================="
+  log "INFO" "      Alex's Environment Setup Completed Successfully!     "
+  log "INFO" "==========================================================="
+  log "INFO" "                                                           "
+  log "INFO" "  Do not forget to run these things:                       "
+  log "INFO" "                                                           "
+  log "INFO" "    - NPM login                                            "
+  log "INFO" "    - Setup .npmrc                                         "
+  log "INFO" "    - Setup iTerm2 or Warp                                 "
+  log "INFO" "    - Setup launchd for notes                              "
+  log "INFO" "    - Create a case-sensitive volume                       "
+  log "INFO" "                                                           "
+  log "INFO" "==========================================================="
+}
+
+finish() {
+  cd $HOME
+  display_todo_list
+}
+
 main() {
   check_macos
   print_banner
-#   restore_dotfiles
-#   setup_ohmyzsh
-#   setup_gpg_agent
-#   install_fonts
-#   install_homebrew
-#   log "INFO" "Setup completed successfully!"
+  restore_dotfiles
+  setup_ohmyzsh
+  install_homebrew
+  install_homebrew_packages
+  install_nodejs
+  install_rust
+  reload_zshrc
+  setup_iterm2
+  finish
 }
 
 main "$@"
